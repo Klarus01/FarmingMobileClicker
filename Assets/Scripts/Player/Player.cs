d7f99d459845d3c714b96e411a821f8e7aa87762
Tron103 <msmolinski.2001@wp.pl>
2024-04-20 13:53:25 +0200

    Add basic player UI

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player : MonoBehaviour
 {
+    public Action OnLevelUp;
+    public Action OnGrainExp;
+
     private int level = 1;
 
     private int money;
 
     private int exp;
-    private int expToLevelUp;
+    private int expToLevelUp = 50;
 
     private readonly int EXP_TO_LEVEL_UP_MULTIPLIER = 2;
+
+    public int Exp { get { return exp; } }
+    public int Level { get { return level; } }
+    public int ExpToLevelUp { get { return expToLevelUp; } }
+
+    private void Start()
+    {
+        OnGrainExp?.Invoke();
+    }
 
     public void AddExp(int expToAdd)
     {
         exp += expToAdd;
         TryToLevelUp();
+
+        OnGrainExp?.Invoke();
     }
 
     private void TryToLevelUp()
@@ -29,7 +44,7 @@ public class Player : MonoBehaviour
         level++;
         exp -= expToLevelUp;
         expToLevelUp *= EXP_TO_LEVEL_UP_MULTIPLIER;
-
+        OnLevelUp?.Invoke();
         TryToLevelUp();
     }
 }
