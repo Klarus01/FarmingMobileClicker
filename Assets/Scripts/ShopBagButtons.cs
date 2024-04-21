aba4aebd9504a3fecacd8b11e740007653988a81
Damian4553 <44645512+Damian4553@users.noreply.github.com>
2024-04-21 11:42:58 +0200

    add bag and eq ui
    add script to close and open ui and make simple bag and eq ui

---
+++ Assets/Scripts/ShopBagButtons.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopBagButtons : MonoBehaviour
+{
+    [SerializeField] private GameObject[] uiToClose;
+    [SerializeField] private GameObject[] uiToOpen;
+
+    private Button button;
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        
+        button.onClick.AddListener(UiToClose);
+        button.onClick.AddListener(UiToOpen);
+
+    }
+
+    private void UiToClose()
+    {
+        foreach (var ui in uiToClose)
+        {
+            ui.gameObject.SetActive(false);
+        }
+    }
+
+    private void UiToOpen()
+    {
+        foreach (var ui in uiToOpen)
+        {
+            Debug.Log(ui);
+
+            ui.gameObject.SetActive(true);
+        }
+    }
+}
